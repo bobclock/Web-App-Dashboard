8be4d1300b05e07ae05c6b9fc426359847367431
bobclock <bmb5rjc@gmail.com>
2016-07-05 22:31:34 +0100

    First upload

---
+++ js/app.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Our hawaiian greeting is displayed as soon as the page loads,
+
+    console.log('Aloha');
+
+});
+
+// Variables
+
+
+
+// Create search function
+  // Get search item
+
+var searchTerm = document.getElementById("search").onkeyup;
+alert(searchTerm.value);
+
+
+  // Loop through photo or video titles
+  // Match search item to titles
+  // Remove of those not matched
+  // Show those matched to search item
+  // Animate movement
+
+
+
+
+
+
+// Filter by videos or photos
+  // Get click of button video or photos
+  // Hide opposite of click content
+  // Show this
+  // Change search options
+
+
+
+
+// Create lightbox function
+  // Get photo being clicked
+  // Show overlay of image
+    // Include background colour
+    // Include image
+    // Include title
+  // Allow scrolling to next image
+    // Get keyboard strike
+    // Get image
+    // Start overlay
+  // Remove overlay
+    // Get click
+    // Hide overlay
